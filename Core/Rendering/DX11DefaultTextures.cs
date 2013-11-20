ef97f6d2dca4c85f95161ade8ae5eac180bae8b4
Julien Vulliet <julien@mrvux.com>
2013-11-21 00:57:26 +0100

    Added default textures

---
+++ Core/Rendering/DX11DefaultTextures.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using FeralTic.DX11.Resources;
+
+namespace FeralTic.DX11
+{
+    public class DefaultTextures : IDisposable
+    {
+        private DX11Device device;
+
+        private IDX11Texture2D blacktex;
+        private IDX11Texture2D whitetex;
+
+        public DefaultTextures(DX11Device device)
+        {
+            this.device = device;
+        }
+
+        public IDX11Texture2D BlackTexture
+        {
+            get
+            {
+                if (this.blacktex == null)
+                {
+                    blacktex = TextureUtils.SolidColor(device, SharpDX.Color4.Black);
+                }
+                return blacktex;
+            }
+        }
+
+        public IDX11Texture2D WhiteTexture
+        {
+            get
+            {
+                if (this.whitetex == null)
+                {
+                    whitetex = TextureUtils.SolidColor(device, SharpDX.Color4.White);
+                }
+                return whitetex;
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.blacktex != null) { this.blacktex.Dispose(); this.blacktex = null; }
+            if (this.whitetex != null) { this.whitetex.Dispose(); this.whitetex = null; }
+        }
+
+    }
+}
---
+++ Core/Utils/TextureUtils.cs
@@ -0,0 +1,41 @@
+﻿using FeralTic.DX11.Resources;
+using SharpDX;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FeralTic.DX11
+{
+    public class TextureUtils
+    {
+        public static DX11Texture2D SolidColor(DX11Device device, Color4 color)
+        {
+            DataStream ds = new DataStream(4, true, true);
+            ds.Write<int>(color.ToRgba());
+
+            Texture2DDescription td = new Texture2DDescription()
+            {
+                ArraySize = 1,
+                BindFlags = BindFlags.ShaderResource,
+                CpuAccessFlags = CpuAccessFlags.None,
+                Format = Format.R8G8B8A8_UNorm,
+                Height = 1,
+                MipLevels = 1,
+                OptionFlags = ResourceOptionFlags.None,
+                SampleDescription = new SampleDescription(1, 0),
+                Usage = ResourceUsage.Immutable,
+                Width = 1
+            };
+
+            DataBox db = new DataBox(ds.DataPointer);
+            db.RowPitch = 4;
+            Texture2D texture = new Texture2D(device.Device, td, new DataBox[1] { db });
+            ds.Dispose();
+            ShaderResourceView srv = new ShaderResourceView(device.Device, texture);
+            return DX11Texture2D.FromReference(device, texture, srv);
+        }
+    }
+}
