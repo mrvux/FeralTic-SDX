fd390358753c2a3b11ef46acd7b81350fb72a433
Julien Vulliet <julien@mrvux.com>
2013-12-30 12:42:12 +0100

    Added Geometry FX settings

---
+++ Addons/FeralTic.Addons.RenderLayers/GeometryFX/GeometryFXExtensions.cs
@@ -0,0 +1,72 @@
+﻿using SharpDX.D3DCompiler;
+using SharpDX.Direct3D11;
+using SharpDX.DXGI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeralTic.RenderLayers.GeometryFX
+{
+    public static class GeometryFXExtentions
+    {
+        public static InputElement[] GetStreamOutputLayout(this EffectPass pass, out int vertexsize)
+        {
+            vertexsize = 0;
+            if (pass.GeometryShaderDescription.Variable == null)
+            {
+                return new InputElement[0];
+            }
+            else
+            {
+                EffectShaderVariable gs = pass.GeometryShaderDescription.Variable;
+                int outputcount = gs.GetShaderDescription(0).OutputParameterCount;
+
+                InputElement[] elems = new InputElement[outputcount];
+
+                int offset = 0;
+
+                for (int vip = 0; vip < outputcount; vip++)
+                {
+                    ShaderParameterDescription sd = gs.GetOutputSignatureElementDescription(0, vip);
+                    int componentcount = 0;
+
+                    if (sd.UsageMask.HasFlag(RegisterComponentMaskFlags.ComponentX)) { componentcount++; }
+                    if (sd.UsageMask.HasFlag(RegisterComponentMaskFlags.ComponentY)) { componentcount++; }
+                    if (sd.UsageMask.HasFlag(RegisterComponentMaskFlags.ComponentZ)) { componentcount++; }
+                    if (sd.UsageMask.HasFlag(RegisterComponentMaskFlags.ComponentW)) { componentcount++; }
+
+                    int vsize = 4 * componentcount;
+
+                    string fmt = "";
+                    if (componentcount == 1) { fmt = "R32_"; }
+                    if (componentcount == 2) { fmt = "R32G32_"; }
+                    if (componentcount == 3) { fmt = "R32G32B32_"; }
+                    if (componentcount == 4) { fmt = "R32G32B32A32_"; }
+
+                    switch (sd.ComponentType)
+                    {
+                        case RegisterComponentType.Float32:
+                            fmt += "Float";
+                            break;
+                        case RegisterComponentType.SInt32:
+                            fmt += "SInt";
+                            break;
+                        case RegisterComponentType.UInt32:
+                            fmt += "UInt";
+                            break;
+                    }
+
+                    Format f = (Format)Enum.Parse(typeof(Format), fmt);
+                    InputElement elem = new InputElement(sd.SemanticName, (int)sd.SemanticIndex, f, offset, 0);
+                    elems[vip] = elem;
+                    offset += vsize;
+                    vertexsize += vsize;
+                }
+
+                return elems;
+            }
+        }
+    }
+}
---
+++ Addons/FeralTic.Addons.RenderLayers/GeometryFX/GeometryFXSettings.cs
@@ -0,0 +1,142 @@
+﻿using FeralTic.DX11;
+using FeralTic.DX11.Resources;
+using SharpDX;
+using SharpDX.Direct3D11;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeralTic.RenderLayers.GeometryFX
+{
+    public class GeometryFXSettings : IViewProjectionSettings
+    {
+        public RenderDevice Device { get; set; }
+
+        public Matrix View { get; set; }
+        public Matrix Projection { get; set; }
+        public Matrix ViewProjection { get; set; }
+
+        public bool AsAuto { get; set; }
+        public bool UseMaxElements { get; set; }
+        public int MaxElements { get; set; }
+
+        private int outputvertexcount;
+        private IDxGeometry outputgeometry;
+        private DX11RawBuffer rawbuffer;
+        private StreamOutputBufferBinding[] binding;
+
+        private InputElement[] outputlayout;
+        private int outputvertexsize;
+
+        public void AssignPass(EffectPass pass)
+        {
+            this.outputlayout = pass.GetStreamOutputLayout(out this.outputvertexsize);
+        }
+
+        public void AssignGeometry(IDxGeometry geometry)
+        {
+            this.outputvertexcount = this.GetVertexCount(geometry);
+            int buffersize = this.outputvertexcount * this.outputvertexsize;
+
+            if (rawbuffer != null)
+            {
+                if (buffersize != this.rawbuffer.Size)
+                {
+                    rawbuffer.Dispose(); rawbuffer = null;
+                }
+            }
+
+            if (rawbuffer == null)
+            {
+                rawbuffer = DX11RawBuffer.CreateWriteable(this.Device, buffersize, new RawBufferBindings() { AllowVertexBuffer = true, AllowStreamOut = true });
+            }
+            this.binding = new StreamOutputBufferBinding[] { new StreamOutputBufferBinding(this.rawbuffer.Buffer, 0) };
+            this.outputgeometry = this.PrepareGeometry(geometry);
+        }
+
+        private int GetVertexCount(IDxGeometry geometry)
+        {
+            if (this.UseMaxElements) { return this.MaxElements; }
+
+            if (geometry is DX11VertexGeometry)
+            {
+                DX11VertexGeometry vd = (DX11VertexGeometry)geometry;
+                return vd.VerticesCount;
+            }
+            if (geometry is DX11IndexedGeometry)
+            {
+                DX11IndexedGeometry id = (DX11IndexedGeometry)geometry;
+                return id.VertexBuffer.VerticesCount;
+            }
+            if (geometry is DX11NullGeometry)
+            {
+                return this.MaxElements;
+            }
+
+            throw new NotSupportedException("Can't set Vertices count from provided geometry");
+        }
+
+        private IDxGeometry PrepareGeometry(IDxGeometry geometry)
+        {
+            if (geometry is DX11VertexGeometry)
+            {
+                DX11VertexGeometry vd = (DX11VertexGeometry)geometry.ShallowCopy();
+                vd.VertexBuffer = this.rawbuffer.Buffer;
+                vd.VertexSize = this.outputvertexsize;
+                vd.VerticesCount = this.outputvertexcount;
+                vd.InputLayout = this.outputlayout;
+                if (this.AsAuto)
+                {
+                    DX11VertexAutoDrawer auto = new DX11VertexAutoDrawer();
+                    vd.AssignDrawer(auto);
+                }
+                return vd;
+            }
+            if (geometry is DX11IndexedGeometry)
+            {
+                if (this.AsAuto)
+                {
+                    DX11VertexGeometry vd = new DX11VertexGeometry(this.Device);
+                    vd.VertexBuffer = this.rawbuffer.Buffer;
+                    vd.VertexSize = this.outputvertexsize;
+                    vd.VerticesCount = this.outputvertexcount;
+                    vd.InputLayout = this.outputlayout;
+                    DX11VertexAutoDrawer auto = new DX11VertexAutoDrawer();
+                    vd.AssignDrawer(auto);
+                    return vd;
+                }
+                else
+                {
+                    //Replace the vertexbuffer
+                    DX11IndexedGeometry id = (DX11IndexedGeometry)geometry.ShallowCopy();
+                    id.InputLayout = this.outputlayout;
+                    id.VertexBuffer = DX11VertexBuffer.CreateFromRawBuffer(this.Device, this.outputvertexcount, this.outputvertexsize, this.rawbuffer);
+                    return id;
+                }
+            }
+            if (geometry is DX11NullGeometry)
+            {
+                DX11VertexGeometry vd = new DX11VertexGeometry(this.Device);
+                vd.VertexBuffer = this.rawbuffer.Buffer;
+                vd.VertexSize = this.outputvertexsize;
+                vd.VerticesCount = this.outputvertexcount;
+                vd.InputLayout = this.outputlayout;
+                if (this.AsAuto)
+                {
+                    DX11VertexAutoDrawer auto = new DX11VertexAutoDrawer();
+                    vd.AssignDrawer(auto);
+                }
+                return vd;
+            }
+
+            throw new NotSupportedException("Can't prepare geometry from provided geometry");
+        }
+
+        public void BindOutput(RenderContext context)
+        {
+            context.Context.StreamOutput.SetTargets(this.binding);
+        }
+    }
+}
