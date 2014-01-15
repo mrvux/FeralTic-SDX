98074fadd440f1656dd4ef4bcaaed5d9d141d2e4
Julien Vulliet <julien@mrvux.com>
2014-01-15 10:03:26 +0100

    Added mediafoundation dll

--- Samples/RenderWindow/Program.cs
+++ Samples/RenderWindow/Program.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using SharpDX.Direct3D11;
 
 namespace RenderWindow
 {
@@ -18,11 +19,40 @@ namespace RenderWindow
         static void Main()
         {
             var renderForm = new RenderForm("");
+
+
+            SharpDX.Direct3D11.Device d = new Device(SharpDX.Direct3D.DriverType.Hardware,DeviceCreationFlags.VideoSupport | DeviceCreationFlags.BgraSupport);
             
 
-            var device = new RenderDevice();
-            var swapChain = new DX11SwapChain(device, renderForm.Handle);
+           /* var device = new RenderDevice(, 1);
             RenderContext context = new RenderContext(device);
+
+            var d = device.Device;*/
+            var multithread = d.QueryInterface<SharpDX.Direct3D.DeviceMultithread>();
+            multithread.SetMultithreadProtected(true);
+
+            // Create a DXGI Device Manager
+            var dxgiDeviceManager = new SharpDX.MediaFoundation.DXGIDeviceManager();
+            dxgiDeviceManager.ResetDevice(d);
+
+            VideoDevice vd = d.QueryInterface<VideoDevice>();
+
+            //vd.VideoDecoderProfileCount
+
+            /*VideoDecoderDescription desc = new VideoDecoderDescription()
+            {
+                
+            }*/
+
+            VideoContext ctx = d.ImmediateContext.QueryInterface<VideoContext>();
+
+            /*var swapChain = new DX11SwapChain(device, renderForm.Handle);
+            
+            /*var dx = new VideoDecoderDescription()
+            {
+                
+            }*/
+            /*int i = vd.VideoDecoderProfileCount;
 
             renderForm.ResizeEnd += (s, e) => swapChain.Resize();
 
@@ -31,7 +61,7 @@ namespace RenderWindow
                 context.Context.ClearRenderTargetView(swapChain.RenderView, new SharpDX.Color4(1, 1, 1, 1));
 
                 swapChain.Present(1, SharpDX.DXGI.PresentFlags.None);
-            });
+            });*/
 
         }
 
