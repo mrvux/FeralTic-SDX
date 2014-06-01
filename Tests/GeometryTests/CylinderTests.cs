a9ff13ee2b0036cd0b91ec0d5d959d2b588e61fd
Julien Vulliet <julien@mrvux.com>
2014-06-01 15:37:56 +0200

    Add tests for standard geometry

---
+++ Tests/GeometryTests/CylinderTests.cs
@@ -0,0 +1,92 @@
+﻿using FeralTic.DX11.Geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeralTic.Tests.GeometryTests
+{
+    public partial class GeometryTests
+    {
+        [TestMethod()]
+        public void ValidCylinder()
+        {
+            var cylinder = new Cylinder()
+            {
+                Caps = true,
+                Cycles = 1.0f,
+                Length = 1.0f,
+                Radius1 = 0.5f,
+                Radius2 = 0.5f,
+                ResolutionX = 16,
+                ResolutionY = 16      
+            };
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidCylinderResolutionX()
+        {
+            var cylinder = new Cylinder()
+            {
+                Caps = true,
+                Cycles = 1.0f,
+                Length = 1.0f,
+                Radius1 = 0.5f,
+                Radius2 = 0.5f,
+                ResolutionX = -5,
+                ResolutionY = 16
+            };
+        }
+
+        [TestMethod()]
+        [ExpectedException(typeof(ArgumentException))]
+        public void InvalidCylinderResolutionY()
+        {
+            var cylinder = new Cylinder()
+            {
+                Caps = true,
+                Cycles = 1.0f,
+                Length = 1.0f,
+                Radius1 = 0.5f,
+                Radius2 = 0.5f,
+                ResolutionX = 16,
+                ResolutionY = -5
+            };
+        }
+
+        [TestMethod()]
+        public void CylinderRaiseEventsProperly()
+        {
+            int count = 0;
+
+            var cylinder = new Cylinder()
+            {
+                Caps = true,
+                Cycles = 1.0f,
+                Length = 1.0f,
+                Radius1 = 0.5f,
+                Radius2 = 0.5f,
+                ResolutionX = 16,
+                ResolutionY = 16
+            };
+
+            cylinder.PropertyChanged += (s, e) => count++;
+            cylinder.Caps = false;
+            cylinder.Cycles = 0.0f;
+            cylinder.Length = 0.5f;
+            cylinder.Radius1 = 0;
+            cylinder.Radius2 = 0;
+            cylinder.ResolutionX = 32;
+            cylinder.ResolutionY = 32;
+
+            Assert.AreEqual<int>(count, 7);
+        }
+
+
+
+
+    }
+}
---
+++ Tests/GeometryTests/PlatonicTests.cs
@@ -0,0 +1,163 @@
+﻿using FeralTic.DX11.Geometry;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeralTic.Tests.GeometryTests
+{
+    public partial class GeometryTests
+    {
+        [TestMethod()]
+        public void ValidDodecaheron()
+        {
+            var solid = new Dodecahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f,1.0f,1.0f)
+            };
+        }
+
+        [TestMethod()]
+        public void DodecaheronRaiseEventsProperly()
+        {
+            int count = 0;
+
+            var solid = new Dodecahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+
+            solid.Size = new SharpDX.Vector3(2.0f, 0.0f, 0.0f);
+            Assert.AreEqual<int>(count, 1);
+        }
+
+        [TestMethod()]
+        public void DodecaheronShouldNotRaiseEvent()
+        {
+            int count = 0;
+
+            var solid = new Dodecahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+
+            solid.Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f);
+            Assert.AreEqual<int>(count, 0);
+        }
+
+        [TestMethod()]
+        public void ValidOctahedron()
+        {
+            var solid = new Octahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+        }
+
+        [TestMethod()]
+        public void OctahedronShouldNotRaiseEvent()
+        {
+            int count = 0;
+
+            var solid = new Octahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+
+            solid.Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f);
+            Assert.AreEqual<int>(count, 0);
+        }
+
+        [TestMethod()]
+        public void OctahedronRaiseEventsProperly()
+        {
+            int count = 0;
+
+            var solid = new Octahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+            solid.Size = new SharpDX.Vector3(2.0f, 0.0f, 0.0f);
+            Assert.AreEqual<int>(count, 1);
+        }
+
+        [TestMethod()]
+        public void ValidTetrahedron()
+        {
+            var solid = new Tetrahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+        }
+
+        [TestMethod()]
+        public void TetrahedronRaiseEventsProperly()
+        {
+            int count = 0;
+            var solid = new Tetrahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+            solid.Size = new SharpDX.Vector3(2.0f, 0.0f, 0.0f);
+            Assert.AreEqual<int>(count, 1);
+        }
+
+        [TestMethod()]
+        public void TetrahedronShouldNotRaiseEvent()
+        {
+            int count = 0;
+
+            var solid = new Tetrahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+            solid.Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f);
+            Assert.AreEqual<int>(count, 0);
+        }
+
+
+        [TestMethod()]
+        public void ValidIsocahedron()
+        {
+            var solid = new Isocahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+        }
+
+        [TestMethod()]
+        public void IsocahedronRaiseEventsProperly()
+        {
+            int count = 0;
+            var solid = new Isocahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+            solid.Size = new SharpDX.Vector3(2.0f, 0.0f, 0.0f);
+            Assert.AreEqual<int>(count, 1);
+        }
+
+        [TestMethod()]
+        public void IsocahedronShouldNotRaiseEvent()
+        {
+            int count = 0;
+
+            var solid = new Isocahedron()
+            {
+                Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f)
+            };
+            solid.PropertyChanged += (s, e) => count++;
+            solid.Size = new SharpDX.Vector3(1.0f, 1.0f, 1.0f);
+            Assert.AreEqual<int>(count, 0);
+        }
+    }
+}
